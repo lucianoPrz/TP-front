cb6395b7c4f6467243303c898aed40f2b63f57b3
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2024-09-02 00:34:03 -0300

    correccion try catch en itemlistcontainer

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,32 +1,32 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
 // import { useParams } from "react-router-dom";
-
-
 
 const ItemDetailContainer = () => {
     const [producto, setProducto] = useState(null);
     const url = "https://fakestoreapi.com/products"; // API BACKEND
-    // const {idItem} = useParams();
-  
-    useEffect(() =>{
-      try {
+    // const { idItem } = useParams();
+
+    useEffect(() => {
         const pedirProducto = async () => {
-          const response = await fetch(`${url}/1`)
-          const data = await response.json();
-          setProducto(data);
-        }
-  
+            try {
+                const response = await fetch(`${url}/1`);
+                if (!response.ok) {
+                    throw new Error("Error al obtener el producto");
+                }
+                const data = await response.json();
+                setProducto(data);
+            } catch (error) {
+                console.log("Error:", error);
+            }
+        };
+
         pedirProducto();
-      } catch (error) {
-        console.log(error);
-      }
+    }, []);
 
-    }, [])
+    return (
+        producto ? <ItemDetail {...producto}/> : <p>Cargando...</p>
+    );
+};
 
-  return (
-    <ItemDetail {...producto}/>
-  )
-}
-
-export default ItemDetailContainer
+export default ItemDetailContainer;
