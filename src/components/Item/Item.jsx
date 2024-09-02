92fda39de1e1f496e71382a96e082b59cf40ccb8
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2024-09-02 17:38:13 -0300

    reacondiciono itemlistcontainer para que pueda renderizar tanto lista de productos como lista de movimientos

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const Item = ({ id, title, price, image }) => {
+const Item = ({ id, title, price, image, tipo, producto, cantidad, costo, fecha, index, dataType }) => {
   return (
-    
-      <tr>
-        <th scope="row">{id}</th>
+    <tr>
+      <th scope="row">{index}</th>
+      {dataType === 'products' && <>
+        <td>{title}</td>
+        <td>${price.toLocaleString()}</td>
+        <td>100 U.</td> {/* Reemplaza con el dato real de stock si lo tienes */}
+      </>}
+      {dataType === 'movements' && <>
+        <td>{title}</td>
         <td>{title}</td>
         <td>{price}</td>
-        <td>100 U.</td>
-      </tr>
-    
-
-
-
+        <td>{tipo === 'entrada' ? `$${costo.toLocaleString()}` : '-'}</td>
+        <td>{new Date("10-10-2024").toLocaleDateString()}</td>
+      </>}
+    </tr>
   );
 };
 
