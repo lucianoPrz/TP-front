e3e05379b76ba7da1fa86c506ccc1e9c71c9f979
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2024-09-06 00:29:23 -0300

    se aplica configuracion para PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        lang: 'es-ES',
+        name: 'Gestion Stock QR',
+        short_name: 'G Stock QR',
+        description: "App de gestion de stock mediante Scanner QR",
+        theme_color: '#D7E0F9',
+        background_color: '#76C0F6',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
