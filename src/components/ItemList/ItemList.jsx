8239bdee61788df5f9e17d5a4499c299bea5d61c
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2024-09-05 22:59:24 -0300

    se aplica responsive a tabla

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,31 +2,33 @@ import Item from "../Item/Item";
 
 const ItemList = ({ items, dataType }) => {
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          {dataType === 'products' && <>
-            <th scope="col">Tipo</th>
-            <th scope="col">Marca</th>
-            <th scope="col">Costo</th>
-            <th scope="col">Stock</th>
-          </>}
-          {dataType === 'movements' && <>
-            <th scope="col">Tipo de Movimiento</th>
-            <th scope="col">Producto</th>
-            <th scope="col">Cantidad</th>
-            <th scope="col">Costo</th>
-            <th scope="col">Fecha</th>
-          </>}
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item, index) => (
-          <Item key={item.id} {...item} index={index + 1} dataType={dataType} />
-        ))}
-      </tbody>
-    </table>
+    <div className="table-responsive">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            {dataType === 'products' && <>
+              <th scope="col">Tipo</th>
+              <th scope="col">Marca</th>
+              <th scope="col">Costo</th>
+              <th scope="col">Stock</th>
+            </>}
+            {dataType === 'movements' && <>
+              <th scope="col">Tipo de Movimiento</th>
+              <th scope="col">Producto</th>
+              <th scope="col">Cantidad</th>
+              <th scope="col">Costo</th>
+              <th scope="col">Fecha</th>
+            </>}
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => (
+            <Item key={item.id} {...item} index={index + 1} dataType={dataType} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
